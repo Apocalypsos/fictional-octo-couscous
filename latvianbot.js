2c11ba861bf0b8fadb20439b217f0255bbfd7e62
Apocalypsos <apokalypso@inbox.lv>
2017-05-23 08:18:38 -0200

    Update latvianbot.js

--- latvianbot.js
+++ latvianbot.js
@@ -240,7 +240,7 @@
         status: false,
         name: "LatvianBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Apocalypsos/fictional-octo-couscous/master/latvianbot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/Apocalypsos/fictional-octo-couscous/master/lv.json",
         chat: null,
@@ -251,7 +251,7 @@
             botName: "LatvianBot",
             language: "english",
             chatLink: "https://rawgit.com/Apocalypsos/fictional-octo-couscous/master/lv.json",
-            scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+            scriptLink: "https://rawgit.com/Apocalypsos/fictional-octo-couscous/master/latvianbot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
